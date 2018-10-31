92b3069e9e06a17ecb593f4d93ee3d9feca6f690
James Traver <jtraver@gogoair.com>
2018-10-30 23:09:20 -0500

    Init react app + starting work to integrate swagger

--- build.sbt
+++ build.sbt
@@ -14,6 +14,7 @@ libraryDependencies ++= Seq(
   "org.scalatra" %% "scalatra" % ScalatraVersion,
   "org.scalatra" %% "scalatra-scalatest" % ScalatraVersion % "test",
   "org.scalatra" %% "scalatra-json" % ScalatraVersion,
+  "org.scalatra" %% "scalatra-swagger"  % "2.6.3",
   "ch.qos.logback" % "logback-classic" % "1.2.3" % "runtime",
   "org.eclipse.jetty" % "jetty-webapp" % "9.4.9.v20180320" % "container",
   "javax.servlet" % "javax.servlet-api" % "3.1.0" % "provided",
---
+++ src/main/scala/com/github/awesome/PlanetsSwagger.scala
@@ -0,0 +1,16 @@
+// package com.playground.awesome
+
+// import org.scalatra.ScalatraServlet
+// import org.scalatra.swagger.{ApiInfo, NativeSwaggerBase, Swagger}
+
+// class ResourcesApp(implicit val swagger: Swagger) extends ScalatraServlet with NativeSwaggerBase
+
+// object PlanetsApiInfo extends ApiInfo(
+//     "The Planets API",
+//     "Docs for the Planets API",
+//     "http://scalatra.org",
+//     "code-for-coffee@github.com",
+//     "MIT",
+//     "http://opensource.org/licenses/MIT")
+
+// class PlanetsSwagger extends Swagger(Swagger.SpecVersion, "1.0.0", PlanetsApiInfo)
